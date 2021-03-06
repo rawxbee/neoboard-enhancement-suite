cac2c33c17c89b9cc97a93039771c301a33fafdf
rawxbee <69788946+rawxbee@users.noreply.github.com>
2021-03-06 06:58:48 -0330

    buff buffer now slightly buff buffier
    buffer extended 10px, fixes unwanted behavior for users without neoboard pens

--- enhanced-neoboard-smilies.user.js
+++ enhanced-neoboard-smilies.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neopets: Enhanced Neoboard Smilies
-// @version      1.3.1
+// @version      1.3.2
 // @description  Adds the entire smilie library to the smilie section of the neoboards. Embeds image links from images.neopets, pets.neopets and upload.neopets as images within replies, a search bar is available to find images.
 // @author       sunbathr & rawbeee
 // @match        http://www.neopets.com/neoboards/create_topic*
@@ -517,7 +517,7 @@ function smile() {
     </div>
   </div>`;
 
-$(`div.container.theme-bg`).append(`<div class="buffbuffer" style="height: 110px;"></div>`);
+$(`div.container.theme-bg`).append(`<div class="buffbuffer" style="height: 120px;"></div>`);
 $(`.replySmilies-neoboards`).html(smilies);
 $(`.topicCreateSmilies-neoboards`).html(smilies);
 }
